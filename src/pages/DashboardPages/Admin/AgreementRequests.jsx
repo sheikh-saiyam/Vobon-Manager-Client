7b67843989b377f5f7370735d02cd22eb47a6a07
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-01-17 19:47:27 +0600

    get all agreement-requests done

--- src/pages/DashboardPages/Admin/AgreementRequests.jsx
+++ src/pages/DashboardPages/Admin/AgreementRequests.jsx
@@ -2,8 +2,27 @@ import { FaCheckSquare } from "react-icons/fa";
 import DashboardContainer from "../../../components/Container/DashboardContainer";
 import SectionHeader from "../../../components/Shared/Section/SectionHeader";
 import { FaCircleXmark } from "react-icons/fa6";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const AgreementRequests = () => {
+  const axiosSecure = useAxiosSecure();
+
+  // Get all agreement requests ----->
+  const {
+    data: agreementRequests = [],
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["agreement-requests"],
+    queryFn: async () => {
+      const { data } = await axiosSecure("/all-agreement-requests");
+      return data;
+    },
+  });
+
+  console.log(agreementRequests);
+
   return (
     <DashboardContainer>
       <SectionHeader
@@ -16,7 +35,8 @@ const AgreementRequests = () => {
         {/* text div */}
         <div className="flex flex-wrap justify-center md:justify-between gap-4 items-center">
           <h1 className="text-xl md:text-2xl lg:text-3xl text-text font-semibold tracking-wide">
-            Total Agreement Requests: <span className="font-medium">4</span>
+            Total Agreement Requests:{" "}
+            <span className="font-medium">{agreementRequests.length}</span>
           </h1>
         </div>
 
@@ -39,52 +59,70 @@ const AgreementRequests = () => {
               </tr>
             </thead>
             <tbody>
-              <tr className="text-base text-text">
-                <td className="py-4">
-                  <h1 className="font-semibold">
-                    Name: <span className="font-medium">Sheikh Saiyam</span>
-                  </h1>
-                  <h1 className="font-semibold">
-                    Email:{" "}
-                    <span className="font-medium text-blue-600 underline underline-offset-2">
-                      sheikhsaiyam@gmail.com
-                    </span>
-                  </h1>
-                </td>
-                <td className="font-semibold">
-                  <h1>
-                    Apartment: <span className="font-medium">K-1102</span>
-                  </h1>
-                  <h1>
-                    Floor Number: <span className="font-medium">11</span>
-                  </h1>
-                  <h1>
-                    Block Name: <span className="font-medium">K</span>
-                  </h1>
-                </td>
-                <td className="font-bold text-3xl tracking-widest">$470</td>
-                <td className="font-semibold tracking-widest">1/16/2025</td>
-                <td className="py-4">
-                  <div className="flex gap-2 items-center">
-                    <div
-                      className="tooltip tooltip-top"
-                      data-tip="Accept Agreement"
-                    >
-                      <button className="bg-accent hover:tooltip-open  text-white font-bold btn w-full">
-                        <FaCheckSquare size={28} />
-                      </button>
+              {agreementRequests.map((agreementRequest) => (
+                <tr key={agreementRequest._id} className="text-base text-text">
+                  <td className="py-4">
+                    <h1 className="font-semibold">
+                      Name:{" "}
+                      <span className="font-medium">
+                        {agreementRequest.user_details.name}
+                      </span>
+                    </h1>
+                    <h1 className="font-semibold">
+                      Email:{" "}
+                      <span className="font-medium text-blue-600 underline underline-offset-2">
+                        {agreementRequest.user_details.email}
+                      </span>
+                    </h1>
+                  </td>
+                  <td className="font-semibold">
+                    <h1>
+                      Apartment:{" "}
+                      <span className="font-medium">
+                        {agreementRequest.apartmentNumber}
+                      </span>
+                    </h1>
+                    <h1>
+                      Floor Number:{" "}
+                      <span className="font-medium">
+                        {agreementRequest.floorNumber}
+                      </span>
+                    </h1>
+                    <h1>
+                      Block Name:{" "}
+                      <span className="font-medium">
+                        {agreementRequest.blockName}
+                      </span>
+                    </h1>
+                  </td>
+                  <td className="font-bold text-3xl tracking-widest">
+                    ${agreementRequest.rent}
+                  </td>
+                  <td className="font-semibold tracking-widest">
+                    {agreementRequest.agreement_request_date.split("T")[0]}
+                  </td>
+                  <td className="py-4">
+                    <div className="flex gap-2 items-center">
+                      <div
+                        className="tooltip tooltip-top"
+                        data-tip="Accept Agreement"
+                      >
+                        <button className="bg-accent hover:tooltip-open  text-white font-bold btn w-full">
+                          <FaCheckSquare size={28} />
+                        </button>
+                      </div>
+                      <div
+                        className="tooltip tooltip-top"
+                        data-tip="Reject Agreement"
+                      >
+                        <button className="bg-red-500 hover:tooltip-open  text-white font-bold btn w-full">
+                          <FaCircleXmark size={28} />
+                        </button>
+                      </div>
                     </div>
-                    <div
-                      className="tooltip tooltip-top"
-                      data-tip="Reject Agreement"
-                    >
-                      <button className="bg-red-500 hover:tooltip-open  text-white font-bold btn w-full">
-                        <FaCircleXmark size={28} />
-                      </button>
-                    </div>
-                  </div>
-                </td>
-              </tr>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
