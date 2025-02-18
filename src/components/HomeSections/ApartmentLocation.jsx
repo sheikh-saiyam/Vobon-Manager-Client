e873ac10094cdff92d06f7534a7d9a94984d40dc
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-02-19 00:46:55 +0600

    apartment location section dark mode added

--- src/components/HomeSections/ApartmentLocation.jsx
+++ src/components/HomeSections/ApartmentLocation.jsx
@@ -4,8 +4,10 @@ import MapImage from "../../assets/map.png";
 const ApartmentLocation = () => {
   return (
     <div>
-      <h3 className="text-lg text-primary">--- Our Location ---</h3>
-      <h1 className="text-text my-2 text-xl md:text-2xl lg:text-3xl font-semibold">
+      <h3 className="text-lg text-primary dark:font-bold">
+        --- Our Location ---
+      </h3>
+      <h1 className="text-text my-2 text-xl md:text-2xl lg:text-3xl font-semibold dark:text-white">
         Find Your Dream Home
       </h1>
 
@@ -24,11 +26,11 @@ const ApartmentLocation = () => {
               <h3 className="text-xl font-semibold text-primary mb-4">
                 How to Get Here
               </h3>
-              <h1 className="mb-3 flex gap-2">
+              <h1 className="mb-3 flex gap-2 dark:text-white">
                 <FaLocationDot className="mt-[4.5px]" /> Sector 7, Uttara,
                 Dhaka-1230, Bangladesh
               </h1>
-              <ul className="list-disc pl-5 space-y-2">
+              <ul className="list-disc pl-5 space-y-2 dark:text-white">
                 <li>Located just 5 minutes from the main highway.</li>
                 <li>Near the popular Mascot Plaza & Sector 7 Park Area.</li>
               </ul>
@@ -39,7 +41,7 @@ const ApartmentLocation = () => {
             <h3 className="text-xl font-semibold text-primary mb-4">
               Nearby Attractions
             </h3>
-            <ul className="list-disc space-y-2 pl-5 text-gray-700">
+            <ul className="list-disc space-y-2 pl-5 text-gray-700 dark:text-white">
               <li>Sector 7 Park - 10 minutes walk</li>
               <li>Sector 13 Park - 10 minutes drive</li>
               <li>Mascot Plaza Shopping Mall - 10 minutes walk</li>
