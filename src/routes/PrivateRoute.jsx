27a62512087b635507784094d9d9faf9c316726c
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-05-02 16:39:44 +0600

    PrivateRoute issue fixed

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,14 +1,16 @@
 import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import Loader from "../components/Loader/Loader";
+import useRole from "../hooks/useRole";
 
 const PrivateRoute = ({ children }) => {
   const location = useLocation();
   const { user, loading } = useAuth();
+  const [, isLoading] = useRole();
 
   if (user && user.email) return children;
 
-  if (loading) return <Loader></Loader>;
+  if (loading || isLoading) return <Loader />;
 
   return <Navigate state={location} to="/login" />;
 };
